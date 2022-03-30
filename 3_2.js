fb73d1c3c2aa49658603b033196ef14cb6f23f04
HOSENMDIMAM <71685504+HOSENMDIMAM@users.noreply.github.com>
2022-03-30 23:26:25 +0900

    create input box

--- 3_2.js
+++ 3_2.js
@@ -1,17 +1,40 @@
 // ボタンタグを取得
-let btn = document.querySelector("button");
-let pr = document.getElementById("pp");
-let input = document.getElementById("input");
+let btn = document.getElementsByTagName("button")[0];
 
 // TODO クリックListenerを追加
-btn.addEventListener("click" ,myfunction);
+// Newinput.value = i
+let i = 1;
+btn.addEventListener('click' ,function(){
+    let p =document.createElement('p')
+    p.textContent = '+1ボタンを押すと、入力欄+1.';
 
-function myfunction(){
-    let Ninput = document.createElement("input");
-    Ninput.setAttribute("type", "text");
-    Ninput.setAttribute("disabled","disabled");
-    pr.appendChild(Ninput);
+    let Newinput = document.createElement('input');
+    Newinput.setAttribute('type', 'text');
+    Newinput.setAttribute('value', i++);
+    Newinput.setAttribute('disabled', 'disabled');
     
+    document.body.appendChild(Newinput);
+    let Newbtn = document.createElement('button');
+    Newbtn.textContent = '+1';
+    Newbtn.setAttribute('id', 'newid');
+    document.body.appendChild(Newbtn);
+    // console.log(Newbtn);
+    document.body.appendChild(p);
 
-    pr.innerHTML += (value = "+1ボタンを押すと、入力欄+1.");
-}
+    // クリックlistener
+    Newbtn.addEventListener('click',function(){
+    
+    let p =document.createElement('p')
+    p.textContent = '+1ボタンを押すと、入力欄+1.';
+
+    let Newinput = document.createElement('input');
+    Newinput.setAttribute('type', 'text');
+    Newinput.setAttribute('value', i++);
+    Newinput.setAttribute('disabled', 'disabled');
+
+    document.body.appendChild(Newinput);
+    document.body.appendChild(Newbtn);
+    document.body.appendChild(p);
+    });
+});
+
