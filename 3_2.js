66a242832a565ef3841af1cd83069f8a980482c7
HOSENMDIMAM <71685504+HOSENMDIMAM@users.noreply.github.com>
2022-04-01 21:14:43 +0900

    Update 3_2.js

--- 3_2.js
+++ 3_2.js
@@ -6,8 +6,9 @@ let btn = document.getElementsByTagName("button")[0];
 let Nt = document.getElementById("input");
 let i = 1;
 btn.addEventListener("click", function(){
-    let New_value = Nt.getAttribute("value");
-     New_value = i++;
+    let New_value = number(Nt.getAttribute("value"));
+    New_value++
+     
     // Nt.setAttribute
     // New_value.innerHTML;
 
